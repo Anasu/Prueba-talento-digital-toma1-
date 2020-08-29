05ec367f4b32bc7516fd95053a556c8663dcd020
anasu <misushima@gmail.com>
2020-08-29 10:53:34 -0400

    Se agregan rutas base

---
+++ src/views/Not-Found.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Ups! Ocurrió un error</h1>
+    <p>No pudimos encontrar el sitio que buscas. Por favor, intenta volviendo al <router-link to="/">Inicio</router-link>.</p>
+  </div>
+</template>
